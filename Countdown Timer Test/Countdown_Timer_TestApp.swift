2efe5e141425f43ccea1c8794f01f52035de497b
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-01 13:33:00 -0600

    Initial Commit

---
+++ Countdown Timer Test/Countdown_Timer_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countdown_Timer_TestApp.swift
+//  Countdown Timer Test
+//
+//  Created by Jack Kamaryt on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Countdown_Timer_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
