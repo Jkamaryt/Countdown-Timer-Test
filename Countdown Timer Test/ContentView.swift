a8bf9f48e84e01ef67e6beb5cd0b12e8c779badd
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-10 10:21:23 -0600

    timer test

--- Countdown Timer Test/ContentView.swift
+++ Countdown Timer Test/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var minutesRemaining = 59
+    @State private var timeRemaining = 59
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Text("Minutes: \(minutesRemaining)")
+                    Text("Seconds: \(timeRemaining)")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .background(.black.opacity(0.75))
+                        .clipShape(Capsule())
+                }
+                
+                ZStack {
+                    
+                }
+                .onReceive(timer) {time in
+                    guard isActive else {return}
+                    
+                    if timeRemaining > 0
+                    {
+                        timeRemaining -= 1
+                    }
+                    if timeRemaining == 0
+                    {
+                        timeRemaining = 59
+                        minutesRemaining -= 1
+                    }
+                }
+                .onChange(of: scenePhase) { newPhase in
+                    if newPhase == .active {
+                        isActive = true
+                    } else {
+                        isActive = false
+                    }
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
